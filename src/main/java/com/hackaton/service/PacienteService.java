c018fb3d043a733a7686a1d13cb4eefd8d9d537f
ALYSSON SANTOS <160049428+Alyssito@users.noreply.github.com>
2025-03-31 21:09:42 -0300

    verificando cpf

--- src/main/java/com/hackaton/service/PacienteService.java
+++ src/main/java/com/hackaton/service/PacienteService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,9 +46,14 @@ public class PacienteService {
             throw new ConflitoException("CPF já cadastrado");
         }
 
-        if (!isValidEmail(pacienteDTO.email())){
+        if (!isValidEmail(pacienteDTO.email())) {
             throw new IllegalArgumentException("Email inválido");
         }
+
+        if (!isValidCPF(pacienteDTO.cpf())) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+
         var novoPaciente = new Paciente();
         novoPaciente.setNome(pacienteDTO.nome());
         novoPaciente.setCpf(pacienteDTO.cpf());
@@ -57,39 +63,78 @@ public class PacienteService {
 
         pacienteRepository.save(novoPaciente);
     }
+
     private boolean isValidEmail(String email) {
-        String regex  = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
 
     }
-    public void removerPaciente(Long id) {
 
-        if (!pacienteRepository.existsById(id)) {
-            throw new IllegalArgumentException("Paciente não encontrado.");
-        }
-        try {
-            pacienteRepository.deleteById(id);
-            log.info("Paciente com id {} removido com sucesso",id);
-        }catch (Exception e){
-            log.error("Erro ao remover paciente com id {}: {}",id, e.getMessage());
-            throw new RuntimeException("Erro ao remover paciente",e);
+    public boolean isValidCPF(String cpf) {
+        // Remover caracteres não numéricos (pontos, traços)
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        // Verificar se o CPF tem 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
         }
 
+        // Verifica se o CPF é composto por números repetidos
+        if (cpf.equals(cpf.substring(0, 1) + cpf.substring(0, 1) + cpf.substring(0, 1) + cpf.substring(0, 1))) {
+            return false;
+        }
+
+        // Calcular o primeiro dígito verificador
+        int soma1 = 0;
+        for (int i = 0; i < 9; i++) {
+            soma1 += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int digito1 = (soma1 * 10) % 11;
+        if (digito1 == 10) {
+            digito1 = 0;
+        }
+
+        // Calcular o segundo dígito verificador
+        int soma2 = 0;
+        for (int i = 0; i < 10; i++) {
+            soma2 += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int digito2 = (soma2 * 10) % 11;
+        if (digito2 == 10) {
+            digito2 = 0;
+        }
+
+        // Verificar se os dois dígitos verificadores calculados são iguais aos fornecidos
+        return cpf.charAt(9) == (digito1 + '0') && cpf.charAt(10) == (digito2 + '0');
     }
+        public void removerPaciente (Long id){
 
-    public Paciente alteraPaciente(PacienteDTO pacienteDTO) {
+            if (!pacienteRepository.existsById(id)) {
+                throw new IllegalArgumentException("Paciente não encontrado.");
+            }
+            try {
+                pacienteRepository.deleteById(id);
+                log.info("Paciente com id {} removido com sucesso", id);
+            } catch (Exception e) {
+                log.error("Erro ao remover paciente com id {}: {}", id, e.getMessage());
+                throw new RuntimeException("Erro ao remover paciente", e);
+            }
 
-        Paciente pacienteExistente = pacienteRepository.findByCpf(pacienteDTO.cpf());
-        if (pacienteExistente == null) {
-            throw new IllegalArgumentException("Paciente não encontrado com o CPF informado.");
         }
 
-        pacienteExistente.setNome(pacienteDTO.nome());
-        pacienteExistente.setCpf(pacienteDTO.cpf());
-        pacienteExistente.setEmail(pacienteDTO.email());
+        public Paciente alteraPaciente (PacienteDTO pacienteDTO){
 
-        return pacienteRepository.save(pacienteExistente);
+            Paciente pacienteExistente = pacienteRepository.findByCpf(pacienteDTO.cpf());
+            if (pacienteExistente == null) {
+                throw new IllegalArgumentException("Paciente não encontrado com o CPF informado.");
+            }
+
+            pacienteExistente.setNome(pacienteDTO.nome());
+            pacienteExistente.setCpf(pacienteDTO.cpf());
+            pacienteExistente.setEmail(pacienteDTO.email());
+
+            return pacienteRepository.save(pacienteExistente);
+        }
     }
-}
